94e6c607825c246f735f96019599429a43349829
nickmorales <nmorales3142@gmail.com>
2019-04-26 15:27:51 -0400

    ft

--- ImageProcess/FourierTransform.py
+++ ImageProcess/FourierTransform.py
@@ -41,22 +41,22 @@ dft_shift = np.fft.fftshift(dft)
 
 magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image 1'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum 1'), plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.subplot(121),plt.imshow(img, cmap = 'gray')
+# plt.title('Input Image 1'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+# plt.title('Magnitude Spectrum 1'), plt.xticks([]), plt.yticks([])
+# plt.show()
 
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
 magnitude_spectrum = 20*np.log(np.abs(fshift))
-print(magnitude_spectrum)
+#print(magnitude_spectrum)
 
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image 2'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum 2'), plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.subplot(121),plt.imshow(img, cmap = 'gray')
+# plt.title('Input Image 2'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+# plt.title('Magnitude Spectrum 2'), plt.xticks([]), plt.yticks([])
+# plt.show()
 
 rows, cols = img.shape
 crow,ccol = rows/2 , cols/2
@@ -67,14 +67,14 @@ f_ishift = np.fft.ifftshift(fshift)
 img_back = np.fft.ifft2(f_ishift)
 img_back = np.abs(img_back)
 
-plt.subplot(131),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
-plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
-plt.subplot(133),plt.imshow(img_back)
-plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+# plt.subplot(131),plt.imshow(img, cmap = 'gray')
+# plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
+# plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+# plt.subplot(133),plt.imshow(img_back)
+# plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+# plt.show()
+# 
+# cv2.waitKey(0)
 
-plt.show()
-
-cv2.waitKey(0)
-
+plt.imsave(outfile_path, img_back)
