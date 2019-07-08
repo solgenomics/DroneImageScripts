44f9112888bd474d27ec85826a5c137048465ae9
nickmorales <nmorales3142@gmail.com>
2019-07-08 14:56:38 -0400

    test

--- ImageProcess/micasense/imageutils.py
+++ ImageProcess/micasense/imageutils.py
@@ -157,7 +157,7 @@ def align(pair):
                 plotutils.plotwithcolorbar(grad2, "match grad level {}".format(level))
                 print("Starting warp for level {} is:\n {}".format(level,warp_matrix))
 
-            cc, warp_matrix = cv2.findTransformECC(grad1, grad2, warp_matrix, warp_mode, criteria)
+            cc, warp_matrix = cv2.findTransformECC(grad1, grad2, warp_matrix, warp_mode, criteria, inputMask = None)
 
             if show_debug_images:
                 print("Warp after alignment level {} is \n{}".format(level,warp_matrix))
