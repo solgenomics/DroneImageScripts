478244594196f78a99fe957b12cb334a59a54c1a
nickmorales <nmorales3142@gmail.com>
2019-07-08 22:21:01 -0400

    enhance image

--- ImageProcess/AlignImages.py
+++ ImageProcess/AlignImages.py
@@ -164,6 +164,10 @@ def run():
             i1 = im_aligned[:,:,[0,1,2]]
             image1 = np.uint8(i1*255)
             image1 = enhance_image(image1)
+            print(image1)
+            print(image1.dtype)
+            print(image1.min())
+            print(image1.max())
             images_to_stich1.append(image1)
 
             i2 = im_aligned[:,:,[2,3,4]]
