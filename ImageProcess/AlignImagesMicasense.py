e928e6df435837b6d1b81732186e1eacf52fe0b7
nickmorales <nmorales3142@gmail.com>
2019-07-23 15:29:05 -0400

    num images

--- ImageProcess/AlignImagesMicasense.py
+++ ImageProcess/AlignImagesMicasense.py
@@ -262,6 +262,7 @@ def run():
     sep = " ";
     images_string1 = sep.join(images_to_stitch1)
     images_string2 = sep.join(images_to_stitch2)
+    num_images = len(images_to_stitch1)
 
     del imageNamesAll
     del imageTempNames
@@ -272,7 +273,7 @@ def run():
     del images_to_stitch1
     del images_to_stitch2
 
-    stitchCmd = "stitching_multi "+images_string1+" "+images_string2+" --num_images "+str(len(images_to_stitch1))+" --result1 '"+final_rgb_output_path+"' --result2 '"+final_rnre_output_path+"' --log_file "+log_file_path+" --work_megapix "+work_megapix+" --ba_refine_mask "+ba_refine_mask
+    stitchCmd = "stitching_multi "+images_string1+" "+images_string2+" --num_images "+str(num_images)+" --result1 '"+final_rgb_output_path+"' --result2 '"+final_rnre_output_path+"' --log_file "+log_file_path+" --work_megapix "+work_megapix+" --ba_refine_mask "+ba_refine_mask
     # stitchCmd = "stitching_multi "+images_string1+" "+images_string2+" --num_images "+str(len(images_to_stitch1))+" --result1 '"+final_rgb_output_path+"' --result2 '"+final_rnre_output_path+"' --try_cuda yes --log_file "+log_file_path+" --work_megapix "+work_megapix
     if log_file_path is not None:
         eprint(stitchCmd)
