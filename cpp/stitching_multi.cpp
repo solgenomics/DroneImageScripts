7fd3007d259f55e85dbf7852ec4b1918f47743da
nickmorales <nmorales3142@gmail.com>
2019-07-25 11:26:51 -0400

    filtered img

--- cpp/stitching_multi.cpp
+++ cpp/stitching_multi.cpp
@@ -589,6 +589,7 @@ int main(int argc, char* argv[])
 
     // Check if we still have enough images
     num_images = static_cast<int>(img_names.size());
+    LOGLN("Number of images: " << num_images);
     if (num_images < 2)
     {
         LOGLN("Need more images");
