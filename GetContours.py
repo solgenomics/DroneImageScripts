74c70ae67be9c73e7d61a634646b02dcd611b275
nickmorales <nmorales3142@gmail.com>
2019-08-21 16:54:21 -0400

    changing get contours

--- GetContours.py
+++ GetContours.py
@@ -6,7 +6,6 @@ import argparse
 import imutils
 import cv2
 import numpy as np
-import urllib.request
 import math
 from ImageContours.ShapeDetection.ShapeDetector import ShapeDetector
 from ImageCropping.CropPolygons.CropPolygonsToSingleImage import CropPolygonsToSingleImage
@@ -14,60 +13,66 @@ import csv
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image_url", required=True, help="url path the link")
+ap.add_argument("-i", "--image_path", required=True, help="url path the link")
 ap.add_argument("-o", "--outfile_path", required=True, help="file path directory where the output will be saved")
 ap.add_argument("-r", "--results_outfile_path", required=False, help="file path where results will be saved")
 args = vars(ap.parse_args())
 
-input_image_url = args["image_url"]
+input_image_path = args["image_path"]
 outfile_path = args["outfile_path"]
 results_outfile_path = args["results_outfile_path"]
 
-with urllib.request.urlopen(input_image_url) as url_response:
-    image = np.asarray(bytearray(url_response.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    #image = cv2.resize(image,(480, 320))
+image = cv2.imread(input_image_path, cv2.IMREAD_UNCHANGED)
 
-imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-imgray = cv2.blur(imgray,(15,15))
+lower = [180, 180, 180]
+upper = [255, 255, 255]
 
-#ret,thresh = cv2.threshold(imgray, math.floor(np.average(imgray)), 255, cv2.THRESH_BINARY_INV)
-#thresh = cv2.adaptiveThreshold(imgray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-thresh = cv2.adaptiveThreshold(imgray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-#cv2.imshow("Threshold", thresh)
+# create NumPy arrays from the boundaries
+lower = np.array(lower, dtype="uint8")
+upper = np.array(upper, dtype="uint8")
 
-#dilated=cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT,(7,7)))
-dilated=cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10)))
-#cv2.imshow("Dilated", dilated)
+mask = cv2.inRange(image, lower, upper)
+output = cv2.bitwise_and(image, image, mask=mask)
+
+ret,thresh = cv2.threshold(mask, 40, 255, 0)
+#dilated=cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15)))
 
 if imutils.is_cv3():
-    im2, contours, hierarchy = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 else:
-    contours, hierarchy = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-print(hierarchy)
+#print(hierarchy)
 ratio = 1
 sd = ShapeDetector()
 
-for i in range(len(contours)):
-    c = contours[i]
-    h = hierarchy[0]
-    M = cv2.moments(c)
-    cX = int((M["m10"] / M["m00"]) * ratio)
-    cY = int((M["m01"] / M["m00"]) * ratio)
-    shape = sd.detect(c)
-    print(shape)
- 
-    if shape == 'circle':
-        print(h[i])
-        c = c.astype("float")
-        c *= ratio
-        c = c.astype("int")
-        cv2.drawContours(image, contours, i, (0, 255, 0))
+c = max(contours, key = cv2.contourArea)
+
+x,y,w,h = cv2.boundingRect(c)
+hull = [cv2.convexHull(c)]
+cv2.drawContours(output, hull, -1, (0,255,0), 2)
+
+#cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+
+# for i in range(len(contours)):
+#     c = contours[i]
+#     h = hierarchy[0]
+#     M = cv2.moments(c)
+#     cX = int((M["m10"] / M["m00"]) * ratio)
+#     cY = int((M["m01"] / M["m00"]) * ratio)
+#     shape = sd.detect(c)
+#     print(shape)
+# 
+#     if shape == 'circle':
+#         print(h[i])
+#         c = c.astype("float")
+#         c *= ratio
+#         c = c.astype("int")
+#         cv2.drawContours(image, contours, i, (255, 255, 0))
         # cv2.imshow("Img"+str(i), image)
         # cv2.waitKey(0)
 
-cv2.imwrite(outfile_path, image)
+cv2.imwrite(outfile_path, output)
 
 result_file_lines = [
     [len(contours)]
