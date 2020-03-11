4118175d7865765206a9e386e3311baa8693a438
nickmorales <nmorales3142@gmail.com>
2020-03-11 17:40:07 -0400

    thin out images in dir

--- ImageProcess/ThinOutImages.py
+++ ImageProcess/ThinOutImages.py
@@ -16,14 +16,26 @@ args = vars(ap.parse_args())
 
 image_dir = args["image_dir"]
 image_out_dir = args["image_out_dir"]
-thin = args["thin"]
+thin = int(args["thin"])
 
-image_names = glob.glob(os.path.join(image_dir,'*.tif'))
+blue_image_names = glob.glob(os.path.join(image_dir,'*1.tif'))
+green_image_names = glob.glob(os.path.join(image_dir,'*2.tif'))
+red_image_names = glob.glob(os.path.join(image_dir,'*3.tif'))
+nir_image_names = glob.glob(os.path.join(image_dir,'*4.tif'))
+red_edge_image_names = glob.glob(os.path.join(image_dir,'*5.tif'))
 
 if not os.path.exists(image_out_dir):
     os.makedirs(image_out_dir)
 
-thinned = image_names[0::thin]
-for img in thinned:
+thinned_blue = blue_image_names[0::thin]
+thinned_green = green_image_names[0::thin]
+thinned_red = red_image_names[0::thin]
+thinned_nir = nir_image_names[0::thin]
+thinned_red_edge = red_edge_image_names[0::thin]
+
+thinned_imgs_list = [thinned_blue, thinned_green, thinned_red, thinned_nir, thinned_red_edge]
+thinned_imgs = [y for x in thinned_imgs_list for y in x]
+
+for img in thinned_imgs:
     name = os.path.basename(img)
     copyfile(img, os.path.join(image_out_dir, name))
