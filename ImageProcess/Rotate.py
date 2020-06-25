81e9a10c2bc4b613f783a28be947fcf4c727bf91
nickmorales <nmorales3142@gmail.com>
2020-06-25 09:35:13 -0400

    rotation working

--- ImageProcess/Rotate.py
+++ ImageProcess/Rotate.py
@@ -13,16 +13,22 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image_path", required=True, help="image path")
 ap.add_argument("-o", "--outfile_path", required=True, help="file path directory where the output will be saved")
 ap.add_argument("-a", "--angle", required=True, help="angle of rotation. positive is counter-clockwise and negative is clockwise")
+ap.add_argument("-c", "--centered", required=False, help="whether to rotate around the center")
 args = vars(ap.parse_args())
 
 input_image = args["image_path"]
 outfile_path = args["outfile_path"]
+centered = args["centered"]
 angle = float(args["angle"])
 
 img = cv2.imread(input_image)
 rows,cols,d = img.shape
 
-M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+center = (cols/2,rows/2)
+if centered:
+    center = (0,0)
+
+M = cv2.getRotationMatrix2D(center,angle,1)
 dst = cv2.warpAffine(img,M,(cols,rows))
 
 #cv2.imshow("Result", dst)
