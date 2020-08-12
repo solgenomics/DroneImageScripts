37c9cc8e6ad79547a7a757abe99d30fe2a0e28ce
nickmorales <nmorales3142@gmail.com>
2020-08-12 10:25:21 -0400

    open and split multi-channel ortho from open data map

---
+++ ImageProcess/ODMOpenImage.py
@@ -0,0 +1,45 @@
+# USAGE
+# python /home/nmorales/cxgn/DroneImageScripts/ImageProcess/ODMOpenImage.py --image_path /folder/mypic.tif --outfile_path_b1 /export/b1.png --outfile_path_b2 /export/b2.png --outfile_path_b3 /export/b3.png --outfile_path_b4 /export/b4.png --outfile_path_b5 /export/b5.png
+
+# import the necessary packages
+import argparse
+import numpy as np
+import math
+import cv2
+import rasterio
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image_path", required=True, help="image path")
+ap.add_argument("-o", "--outfile_path_b1", required=True, help="where output image will be saved for band 1")
+ap.add_argument("-j", "--outfile_path_b2", required=True, help="where output image will be saved for band 2")
+ap.add_argument("-k", "--outfile_path_b3", required=True, help="where output image will be saved for band 3")
+ap.add_argument("-l", "--outfile_path_b4", required=True, help="where output image will be saved for band 4")
+ap.add_argument("-m", "--outfile_path_b5", required=True, help="where output image will be saved for band 5")
+args = vars(ap.parse_args())
+
+input_image = args["image_path"]
+outfile_path_b1 = args["outfile_path_b1"]
+outfile_path_b2 = args["outfile_path_b2"]
+outfile_path_b3 = args["outfile_path_b3"]
+outfile_path_b4 = args["outfile_path_b4"]
+outfile_path_b5 = args["outfile_path_b5"]
+
+print(input_image)
+dataset = rasterio.open(input_image)
+print(dataset.count)
+
+band1 = dataset.read(1)
+band2 = dataset.read(2)
+band3 = dataset.read(3)
+band4 = dataset.read(4)
+band5 = dataset.read(5)
+
+#cv2.imshow("Result", band1.ReadAsArray())
+#cv2.waitKey(0)
+
+cv2.imwrite(outfile_path_b1, band1)
+cv2.imwrite(outfile_path_b2, band2)
+cv2.imwrite(outfile_path_b3, band3)
+cv2.imwrite(outfile_path_b4, band4)
+cv2.imwrite(outfile_path_b5, band5)
