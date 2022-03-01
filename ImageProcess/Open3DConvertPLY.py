1998062efeec114a4f4156bbf6834e4ea4d907dd
nickmorales <nmorales3142@gmail.com>
2022-03-01 16:47:51 -0500

    save verts

--- ImageProcess/Open3DConvertPLY.py
+++ ImageProcess/Open3DConvertPLY.py
@@ -13,12 +13,14 @@ ap.add_argument("-i", "--input_ply_path", required=True, help="point cloud (.ply
 ap.add_argument("-o", "--output_obj_path", required=True, help="output file (.obj) for point cloud")
 ap.add_argument("-p", "--output_pcd_path", required=True, help="output file (.pcd) for point cloud")
 ap.add_argument("-q", "--output_gltf_path", required=True, help="output file (.gltf) for point cloud")
+ap.add_argument("-r", "--output_vertices_path", required=True, help="output file (.csv) for point cloud vertices")
 args = vars(ap.parse_args())
 
 input_ply_path = args["input_ply_path"]
 output_obj_path = args["output_obj_path"]
 output_pcd_path = args["output_pcd_path"]
 output_gltf_path = args["output_gltf_path"]
+output_vertices_path = args["output_vertices_path"]
 
 pcd = o3d.io.read_point_cloud(input_ply_path)
 o3d.io.write_point_cloud(output_pcd_path, pcd)
@@ -26,3 +28,12 @@ o3d.io.write_point_cloud(output_pcd_path, pcd)
 mesh = o3d.io.read_triangle_mesh(input_ply_path)
 o3d.io.write_triangle_mesh(output_obj_path, mesh)
 o3d.io.write_triangle_mesh(output_gltf_path, mesh)
+
+verts = mesh.vertices
+# print(verts)
+
+with open(output_vertices_path, 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(verts)
+
+writeFile.close()
